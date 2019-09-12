202a1c88280358853733dfe3342ccfbac59f3da6
kevinjel <kjmbp@kjmbp.local>
2019-09-12 21:01:36 +0200

    Update brewfile with new apps, gnew in alias Lets hope this solves the googling for git init issues on local vs remote

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,54 +3,54 @@
 # os conditional aliasses;
 if [[ "$(uname)" == "Linux" ]]; then
     # linux > i have no linux machine anymore ;-)
-    alias pbcopy='xclip -selection clipboard'
+    alias pbcopy="xclip -selection clipboard"
     # test if we are running on WSL
     if [[ "$(uname -r)" =~ Microsoft ]]; then
         # windows linux subsystem
-        alias pbcopy='clip.exe'
+        alias pbcopy="clip.exe"
     fi
 
     # linux global settings
-    alias diskspace='du -h | sort -rn | more'
+    alias diskspace="du -h | sort -rn | more"
 
     # test which package manager is being used
     if [[ ! -z "$(which yum)" ]]; then
-        alias update='sudo yum update'
+        alias update="sudo yum update"
     elif [[ ! -z "$(which apt-get)" ]]; then
-        alias update='sudo apt-get update && sudo apt-get upgrade'
+        alias update="sudo apt-get update && sudo apt-get upgrade"
     fi
 
 elif [[ "$(uname)" == "Darwin" ]]; then
     # macosx
     dotfileLocation="$HOME/dotfiles"
-    alias diskspace='du -hxs | sort -rn'
-    alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; brew cask upgrade && mkBrewfile'
-    alias mkBrewfile='rm $dotfileLocation/brew/Brewfile && brew bundle dump && mv ./Brewfile $dotfileLocation/brew/'
-    alias cat='bat'
+    alias diskspace="du -hxs | sort -rn"
+    alias update="brew update; brew upgrade; brew cleanup; brew cask upgrade && mkBrewfile"
+    alias mkBrewfile="rm $dotfileLocation/brew/Brewfile && brew bundle dump && mv ./Brewfile $dotfileLocation/brew/"
+    alias cat="bat"
 
 else
     # ...
-    echo 'unknown OS! Running: ' "$(uname -a)"
+    echo "unknown OS! Running: " "$(uname -a)"
 fi
 
 # use neovim when available
 if type nvim > /dev/null 2>&1; then
-  alias vim='nvim'
+  alias vim="nvim"
 fi
 
 # use unicode in tmux
-alias tmux='tmux -u'
-alias tma='tmux attach -t' 
-alias tmk='tmux kill-session -t' 
+alias tmux="tmux -u"
+alias tma="tmux attach -t" 
+alias tmk="tmux kill-session -t" 
 
 # list all ze things
 alias ll="ls -lha"
 alias wls="watch ls -lhat"
 
 # cd navigation
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
 
 # grep colors
 alias grep="grep --color=auto"
@@ -58,15 +58,19 @@ alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
 
 # get week number
-alias week='date +%V'
+alias week="date +%V"
 
 # ping standard 5 times
-alias ping='ping -c 5'
+alias ping="ping -c 5"
 
 # protect against overwriting
-alias cp='cp -i'
-alias mv='mv -i'
+alias cp="cp -i"
+alias mv="mv -i"
 
 # an incognito bash without history (; 
-alias incognito='bash --init-file <(echo ". ~/.bashrc; unset HISTFILE")'
-alias top='htop' # set top to htop
+alias incognito="bash --init-file <(echo '. ~/.bashrc; unset HISTFILE')"
+alias top="htop" # set top to htop
+
+# when doing local git init then try to commit it, it gives errors
+# this fixes that issue
+alias gnew="git pull --allow-unrelated-histories origin master"
