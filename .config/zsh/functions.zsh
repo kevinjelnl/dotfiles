de6c9943d4000fb77e4865e6c46a8337e89f78bd
kevinjel <ztfe3nvd@duck.com>
2022-12-15 20:06:29 +0100

    add NVIM config file and plugins, update bootstrap to install vim-plug, add tmux aliases

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -5,6 +5,23 @@ function md() {
     mkdir -p "$@" && cd "$_";
 }
 
-function rm-dangling-dockerimages(){
+function rm-dangling-dockerimages() {
     docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+}
+
+function vscode() {
+    # found on: https://koenwoortman.com/tmux-sessions-should-be-nested-with-care-unset-tmux-to-force/
+    session_name="vscode"
+    # 1. First you check if a tmux session exists with a given name.
+    tmux has-session -t=$session_name 2> /dev/null
+    # 2. Create the session if it doesn't exists.
+    if [[ $? -ne 0 ]]; then
+      TMUX='' tmux new-session -d -s "$session_name"
+    fi
+    # 3. Attach if outside of tmux, switch if you're in tmux.
+    if [[ -z "$TMUX" ]]; then
+      tmux attach -t "$session_name"
+    else
+      tmux switch-client -t "$session_name"
+    fi
 }
